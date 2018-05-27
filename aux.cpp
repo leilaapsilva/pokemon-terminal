6bcbffbbe42c030e5f53e08e95e4226232d5f1af
Leila Aparecida da Silva <leilaapsilva26@gmail.com>
2018-05-27 00:58:19 -0300

    MELHOR TIME <3 (jogadores alternados funcionando)

--- aux.cpp
+++ aux.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 typedef int TipoChave;
 
+#define NUM_MOVES 4
+
 typedef struct TipoMove{
 	string nome; 
 	int id;
@@ -25,7 +27,7 @@ typedef struct TipoPokemon{
 	float speed; //velocidade (o maior ataca antes)
 	float sp_atk; //ataque especial
 	float sp_def; //defesa especial
-	TipoMove moves[3]; //movimentos de ataque possiveis
+	TipoMove moves[NUM_MOVES]; //movimentos de ataque possiveis
 } TipoPokemon;
 
 typedef struct TipoNo *TipoApontador;
@@ -37,8 +39,8 @@ typedef struct TipoNo {
 
 typedef struct TipoLista {
 	TipoApontador Primeiro, Ultimo;
+	int player; //player 1 ou 2
 } TipoLista;
-
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -48,52 +50,13 @@ void InicializaMove(TipoMove *mov, string n, int id, char t, float pw){
 	mov->tipo = t;
 	mov->power = pw;
 	
-	cout << "move " << mov->nome << " inicializado " << endl;
-	
+	//cout << "move " << mov->nome << " inicializado " << endl;
 }
-/*
-	const TipoMove flamethrower; 
-	const TipoMove fire_blast; 
-	const TipoMove fly; 
-	const TipoMove earthquake;
-	
-	InicializaMove(&flamethrower, "Flamethrower", 1, 'e', 95); 
 
-*/
-	//moves ---- devem ser const para que possam ser usados no switch
-	
-	//charizard 
-		
-	//const int flamethrower = 1; //special
-	//const int fire_blast = 2; //s
-	//const int fly = 3; //fisico
-	//const int earthquake = 4; //f
-	
-	//pikachu
-	
-	/*
-	const int quick_attack = 5; //f
-	const int thunderbolt = 6; //s
-	const int thunder = 7; //s
-	const int skull_bash = 8; //f
-	
-	//dragonite
-	
-	const int ice_beam = 9; //s
-	
-	//vaporeon
-	
-	const int hydro_pump = 10; //s
-	const int aurora_beam = 11; //s
-	const int blizzard = 12; //s
-
-*/
-
-
-void InicializaLista(TipoLista *L) {
+void InicializaLista(TipoLista *L, int player) {
 	(*L).Primeiro = NULL;
 	(*L).Ultimo = NULL;
-	printf("Criou a lista\n");
+	//printf("Criou a lista\n");
 }
 
 int ListaVazia(TipoLista *L) {
@@ -123,7 +86,7 @@ void InsereLista(TipoLista *L, TipoPokemon poke) {
 		L->Ultimo = P;
 	}
 
-	cout << "Pokemon inserido: " << poke.id << endl;
+	//cout << "Pokemon inserido: " << poke.id << endl;
 }
 
 TipoApontador PesquisaLista(TipoLista *L, TipoChave C) {
@@ -143,6 +106,71 @@ TipoApontador PesquisaLista(TipoLista *L, TipoChave C) {
 	return NULL; 
 }
 
+int static RemoveListaPosicao(TipoLista *L, TipoApontador P) {
+
+	if (P->pokemon.id == -1)
+		return -1;
+
+	// unico elemento na lista
+	if (P == L->Primeiro && L->Primeiro == L->Ultimo) {
+		L->Primeiro = NULL;
+		L->Ultimo = NULL;
+		free(P);
+		return 0;
+	}
+	
+	// Remove do inicio
+	if (P == L->Primeiro) {
+		L->Primeiro = L->Primeiro->prox;
+		free(P);
+		return 0;
+	}
+	
+	// Remove no meio da lista
+	TipoApontador aux = L->Primeiro;
+	while(aux->prox != NULL && aux->prox != P) {
+		aux = aux->prox;
+	}
+	
+	aux->prox = P->prox;
+	// se o removido estava no fim
+	if (aux->prox == NULL)
+		L->Ultimo = aux;
+	
+	free(P);
+	
+	return 0;
+	
+}
+
+void RemoveLista(TipoLista *L, TipoChave C) {	
+	TipoApontador P = PesquisaLista(L, C);	
+	int r = RemoveListaPosicao(L, P);
+	if (r != -1)
+		printf("Removeu elemento\n");
+	else
+		printf("Remocao deu ruim\n");
+}
+
+void ImprimeLista(TipoLista *L) {
+	
+	TipoApontador P = L->Primeiro;
+
+	int i = 0;
+	while(P != NULL) {
+		if (P->prox != NULL){ //se nao for o ultimo, aponta para o proximo
+		cout << L->Primeiro->pokemon.nome << endl;
+		cout << L->Primeiro->prox->pokemon.nome << endl;
+		}else{ //se for o ultimo, apenas imprime
+					
+		}
+		P = P->prox;
+		i++;
+	}
+	
+}
+
+
 TipoPokemon PrimeiroPokemon(TipoLista *L){
 	TipoPokemon ash;
 	ash.id = -1;
@@ -155,7 +183,7 @@ TipoPokemon PrimeiroPokemon(TipoLista *L){
 	return L->Primeiro->pokemon; 
 }
 
-void InicializaPokemon(TipoPokemon *poke, int id, string nome, float ataque, float defesa, float vida, float velocidade, float spAtk, float spDef, TipoMove mov0, TipoMove mov1, TipoMove mov2, TipoMove mov3){			
+void InicializaPokemon(TipoPokemon *poke, int id, string nome, float vida, float ataque, float spAtk, float defesa, float spDef, float velocidade, TipoMove mov0, TipoMove mov1, TipoMove mov2, TipoMove mov3){		
 								
 				poke->id = id;
 				poke->nome = nome;
@@ -168,8 +196,8 @@ void InicializaPokemon(TipoPokemon *poke, int id, string nome, float ataque, flo
 				poke->moves[0]= mov0;
 				poke->moves[1] = mov1;
 				poke->moves[2] = mov2;
-				poke->moves[3] = mov3;			 															
-
+				poke->moves[3] = mov3;		
+																			
 }
 
 void ImprimePokemon(TipoPokemon *poke){
@@ -177,142 +205,101 @@ void ImprimePokemon(TipoPokemon *poke){
 	cout << endl;
 	cout << "****************************" << endl;
 	cout << "*  NOME:	" <<  poke->nome  << "	           *" << endl;
-	cout << "*  ID: " << poke->id << "                   *" << endl;
+//	cout << "*  ID: " << poke->id << "                   *" << endl;
 //	cout << "*  ATK: " << poke->atk << "	           *" << endl;	
 //	cout << "*  DEF: " << poke->def << "	           *" << endl;
 	cout << "*  HP: " << poke->hp << "	           *" << endl;
 	cout << "*  SPEED: " << poke->speed << "	           *" << endl;
 //	cout << "*  SP.ATK: " << poke->sp_atk << "	           *" << endl;
 //	cout << "*  SP.DEF:	" << poke->sp_def << " 	   *" << endl;
-	cout << "*  MOVES: " << "                 *" << endl;
-	cout << "*  "<< poke->moves->nome << "            *" << endl;
-	cout << "*  "<< poke->moves[1].nome << "              *" << endl;
-	cout << "*  "<< poke->moves[2].nome << "                     *" << endl;
-	cout << "*  "<< poke->moves[3].nome << "              *" << endl;
+//	cout << "*  MOVES: " << "                 *" << endl;
+//	cout << "*  "<< poke->moves->nome << "            *" << endl;
+//	cout << "*  "<< poke->moves[1].nome << "              *" << endl;
+//	cout << "*  "<< poke->moves[2].nome << "                     *" << endl;
+//	cout << "*  "<< poke->moves[3].nome << "              *" << endl;
 	cout << "****************************" << endl; 
 } 
 
-void ImprimeMoves(TipoPokemon *poke){
-	for(int i = 0; i < 4; i++){
-		cout << "(" << i+1 << ") ";
-	}
-}
+//retorna 0 caso nao elimine e 1 caso elimine
+int Ataque(TipoPokemon *ataca, TipoPokemon *defende, TipoMove *move){
 
-/*
-void Ataque(TipoPokemon *fez_atk, TipoPokemon *rec_atk, int move){
-	
-	float power_move;
-	int type_move; //0 para fisico e 1 para especial
+	cout << endl;
 	float dano;
-	string string_move;
 	
-	switch(move){
-		case flamethrower: 
-			power_move = 95;
-			type_move = 1; //especial
-			string_move = "flamethrower";
-			break;
-			
-        case fire_blast: 
-        	power_move = 120;
-        	type_move = 1;
-        	string_move = "fire blast";
-			break;
-        	
-	    case fly:
-        	power_move = 70;
-        	type_move = 0;
-        	string_move = "fly";
-			break;
-    	
-		case earthquake:
-        	power_move = 100;
-        	type_move = 0;
-        	string_move = "earthquake";
-			break;
-        	
-		case quick_attack:
-			power_move = 40;
-        	type_move = 0;
-        	string_move = "quick_attack";
-			break;
-        	
-		case thunderbolt:
-			power_move = 95;
-        	type_move = 1;
-        	string_move = "thunderbolt";
-			break;
-			
-		case thunder:
-			power_move = 120;
-        	type_move = 1;
-        	string_move = "thunder";
-			break;
-			
-		case skull_bash:
-			power_move = 100;
-        	type_move = 0;
-        	string_move = "skull bash";
-			break;
-			
-		case ice_beam:
-			power_move = 95;
-        	type_move = 1;
-        	string_move = "ice beam";
-			break;
-			
-		case hydro_pump:
-			power_move = 120;
-        	type_move = 1;
-        	string_move = "hydro pump";
-			break;
-			
-		case aurora_beam:
-			power_move = 65;
-        	type_move = 1;
-        	string_move = "aurora beam";
-			break;
-			
-		case blizzard:    	
-  			power_move = 120;
-        	type_move = 1;
-        	string_move = "blizzard";
-			break;
-	}
-	
-	if(type_move == 1){ //ataques especiais atingem defesa especial
-		dano = ((fez_atk->sp_atk / rec_atk->sp_def) * power_move)/2; 
-		
-	
-		
-	} else{ //ataques fisicos atingem a defesa normal
-	
-		dano = ((fez_atk->atk / rec_atk->def) * power_move)/2;
+	//cout <<"Tipo do move usado: " << move->tipo << endl;
 
+	if(move->tipo == 'e'){ //ataque especial atinge defesa especial
+		//cout << "ataque especial" << endl;
+		dano = ((ataca->sp_atk / defende->sp_def) * move->power)/2;
 	}
-	
+	else if(move->tipo == 'f'){ //ataque fisico atinge defesa normal
+		//cout << "ataque fisico" << endl;
+
+		dano = ((ataca->atk / defende->def) * move->power)/2;	
+	}
+	else{
+		//tipo invalido
+		cout << "tipo invalido" << endl;
+	}
+
 	//trunca o dano para ficar inteiro
 	dano = floor(dano);
 	
-	rec_atk->hp = rec_atk->hp - dano;
-	
-	cout << " *nome pokemon* atacou" << " *nome pokemon* " << " usando " << string_move << " e causou " << dano << " de dano";
-	
+	//cout << "dano: " << dano << endl;
+
+	//decrementa a hp do pokemon que recebeu o ataque
+	defende->hp = defende->hp - dano; 
+
+	cout << ataca->nome << " atacou " << defende->nome << " usando " << move->nome << " e causou " << dano << " de dano"<< endl;
+
+	cout << "hp do atacado = " << defende->hp << endl;
+
+	if(defende->hp <= 0){
+		cout << defende->nome << "eliminado!" << endl;
+		//RemoveLista(lista, chave);
+		return 1;
+	}
+	return 0;
+
 }
 
-*/
+//metodo para escolha do move a ser jogado
+void ImprimeMoves(TipoPokemon *pokemon, TipoMove *escolhido){	
+	
+	int n_escolhido = 0;
+	cout << "Escolha o move: " << endl;
+	for(int i = 0; i < NUM_MOVES; i++){
+		cout << "(" << i+1 << ") " << pokemon->moves[i].nome << endl;
+	}
+	cin >> n_escolhido;
+
+	*escolhido = pokemon->moves[n_escolhido-1];
+}
+
+TipoLista comeca(TipoLista p1, TipoLista p2){
+
+	//se for o primeiro turno
+	//determina qual joga primeiro
+	if(p1.Primeiro->pokemon.speed > p2.Primeiro->pokemon.speed){
+		//pokemon1 comeca
+		cout << endl;
+		cout << "speed do " << p1.Primeiro->pokemon.nome <<" maior, ele comeca" << endl;
+		return p1;
+	
+	} else
+	{
+		//pokemon2 comeca
+		cout << endl;
+		cout << "speed do " << p2.Primeiro->pokemon.nome <<" maior, ele comeca" << endl;
+		return p2;
+	}
+}
 
 int main(int argc, char** argv)
 {
-	TipoMove flamethrower; 
-	TipoMove fire_blast; 
-	TipoMove fly; 
-	TipoMove earthquake;
-	
-	TipoMove quick_attack;
-	TipoMove thunderbolt;
-	TipoMove thunder;
-	TipoMove skull_bash;
+
+//	cout << "Inicializando moves :) " << endl << endl; 
+	TipoMove flamethrower, fire_blast, fly, earthquake, quick_attack, thunderbolt, thunder, skull_bash, ice_beam, hydro_pump, aurora_beam, blizzard; 
 	
 	InicializaMove(&flamethrower, "Flamethrower", 1, 'e', 95); 
 	InicializaMove(&fire_blast, "Fire Blast", 2, 'e', 120);
@@ -323,86 +310,123 @@ int main(int argc, char** argv)
 	InicializaMove(&thunderbolt, "Thunderbolt", 6, 'e', 95);
 	InicializaMove(&thunder, "Thunder", 7, 'e', 120);
 	InicializaMove(&skull_bash, "Skull Bash", 8, 'f', 100);
-		
-	TipoPokemon charizard;
-	TipoPokemon pikachu;
 	
-	cout << "teste" << endl;
+	InicializaMove(&ice_beam, "Ice Beam", 9, 'e', 95);
 	
-	cout <<"teste" << endl;
-	
-		
-	InicializaPokemon(&charizard, 2, "Charizard", 293, 280, 360, 328, 348, 295, flamethrower, fire_blast, fly, earthquake);
-	
-	ImprimePokemon(&charizard);
-										
-	InicializaPokemon(&pikachu, 1, "pikachu", 229, 196, 274, 306, 218, 218, quick_attack, thunderbolt, thunder, skull_bash);
-	
-	ImprimePokemon(&pikachu);
+	InicializaMove(&hydro_pump, "Hydro Pump", 10, 'e', 120);
+	InicializaMove(&aurora_beam, "Aurora Beam", 11, 'e', 65);
+	InicializaMove(&blizzard, "Blizzard", 12, 'e', 120);
 
+
+//	cout << "Inicializando pokemons :) " << endl << endl; 	
 	
+	TipoPokemon pikachu, charizard, dragonite, vaporeon;
+
+		
+	InicializaPokemon(&pikachu, 1, "Pikachu", 274, 229, 218, 196, 218, 306, quick_attack, thunderbolt, thunder, skull_bash);
+
+	InicializaPokemon(&charizard, 2, "Charizard", 360, 293, 348, 280, 295, 328, flamethrower, fire_blast, fly, earthquake);
+
+	InicializaPokemon(&dragonite, 3, "Dragonite", 386, 403, 328, 317, 328, 284, fly, earthquake, ice_beam, fire_blast);
+
+	InicializaPokemon(&vaporeon, 4, "Vaporeon", 464, 251, 350, 240, 317, 251, hydro_pump, aurora_beam, quick_attack, blizzard); 
 	
-	/*
-	//listas de pokemons dos jogadores
 	TipoLista player1, player2;
-	InicializaLista(&player1);
-	InicializaLista(&player2);
-	
-	cout << "player 1 <- pikachu" << endl;
-	cout << "player 2 <- charizard" << endl;
-	
-	InsereLista(&player1, pikachu);
-	InsereLista(&player2, charizard);
 
-	
-	
-	
-	cout<<"pikachu:"<<endl;
-	
-	ImprimePokemon(&pikachu);
-	
-	cout<<"charizard:"<< endl;
-	
-	ImprimePokemon(&charizard);
-	
+	InicializaLista(&player1, 1);
+	InicializaLista(&player2, 2);
+
+	InsereLista(&player1, charizard);
+	InsereLista(&player1, dragonite);	
+
+
+	InsereLista(&player2, vaporeon);	
+	InsereLista(&player2, pikachu);
+
 	//no começo do jogo, pega os primeiros da lista de cada jogador	
 	
 	TipoPokemon pokemon1 = PrimeiroPokemon(&player1);
+	cout << "primeiro do player1: " << pokemon1.nome << endl;
+	
 	TipoPokemon pokemon2 = PrimeiroPokemon(&player2);
+	cout << "primeiro do player2: " << pokemon2.nome << endl;	
+
+	//TipoPokemon pokemonAtual = comeca(pokemon1, pokemon2);
 	
-	//determina qual joga primeiro
-	if(pokemon1.speed > pokemon2.speed){
-		//pokemon1 comeca
+	TipoLista jogadorAtual = comeca(player1, player2);
+	TipoLista proxJogador, aux;
+	
+
+	bool comeco = true;
+	bool acabou = false;
+
+	while(comeco){
+		//proximo jogador
+		if(jogadorAtual.player == player1.player){
+			proxJogador = player2;
+			
+		}
+		else{
+			proxJogador = player1;
+		}
+
+		TipoMove escolhido;	
+
+		ImprimeMoves(&jogadorAtual.Primeiro->pokemon, &escolhido);
+		
+		int eliminado = Ataque(&jogadorAtual.Primeiro->pokemon, &proxJogador.Primeiro->pokemon, &escolhido);		
+
+		if(eliminado){
+			RemoveLista(&proxJogador, proxJogador.Primeiro->pokemon.id); 		
+		}
+
 		cout << endl;
-		cout << "speed do pokemon 1 maior, ele comeca" << endl;
-		
-		
-		
-	} else
-	{
-		//pokemon2 comeca
+
+		ImprimeLista(&jogadorAtual);
 		cout << endl;
-		cout << "speed do pokemon 2 maior, ele comeca" << endl;
+		ImprimeLista(&proxJogador);
+		cout << endl;
+		
+		ImprimeLista(&aux);
+		cout << endl;
+
+		aux = jogadorAtual;
+		jogadorAtual = proxJogador;
+		proxJogador = aux;
+
+		cout << "--------------------------------------------------------------" << endl;
+		
+		comeco = false;
 	}
-	
-	cout << "escolha o move: " << endl;
-	cout << "(1) flamethrower" << endl;
-	cout << "(2) fire blast" << endl;
-	cout << "(3) fly " << endl;
-	cout << "(4) earthquake" << endl;
-	
-	//TO DO - verificar se o move escolhido pertence ao pokemon
-	
-	
-	int mov;
-	
-	cin >> mov;
-	
-	Ataque(&charizard, &pikachu, mov);	
-	
-	ImprimePokemon(&pikachu);
-	
-	*/
+
+	while(!acabou){
+		TipoMove escolhido;	
+
+		ImprimeMoves(&jogadorAtual.Primeiro->pokemon, &escolhido);
+		
+		int eliminado = Ataque(&jogadorAtual.Primeiro->pokemon, &proxJogador.Primeiro->pokemon, &escolhido);		
+
+		if(eliminado){
+			RemoveLista(&proxJogador, proxJogador.Primeiro->pokemon.id); 		
+		}
+
+		cout << endl;
+
+		ImprimeLista(&jogadorAtual);
+		cout << endl;
+		ImprimeLista(&proxJogador);
+		cout << endl;
+		
+		ImprimeLista(&aux);
+		cout << endl;
+
+		aux = jogadorAtual;
+		jogadorAtual = proxJogador;
+		proxJogador = aux;
+
+		cout << "--------------------------------------------------------------" << endl;
+	}
+
 	
 	return 0;
 }
