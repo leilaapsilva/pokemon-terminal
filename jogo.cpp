aa8dfc9b88ed2c7c208ab714db09f77e3912d152
Leila Aparecida da Silva <leilaapsilva26@gmail.com>
2018-05-27 21:23:30 -0300

    adiciona outros pokemon e moves

--- jogo.cpp
+++ jogo.cpp
@@ -4,25 +4,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include <unistd.h>
+#include "pokedexascii.h" //biblioteca com os 151 pokemons iniciais para impressao
+//#include "poketads.h"
 
-#include "pokedexascii.h"
 
 using namespace std;
 
-typedef int TipoChave;
+#define ANSI_COLOR_RED     	"\x1b[31m" //cores em ANSI utilizadas 
+#define ANSI_COLOR_GRAY    	"\e[0;37m"
+#define ANSI_COLOR_DARK_GRAY 	"\e[1;30m"
+#define ANSI_COLOR_GREEN	"\e[0;32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
-#define NUM_MOVES 4
+#define NUM_MOVES 4 //quantidade de moves para 
+
+
+
+//TADs utilizados 
+
+typedef int TipoChave;
 
 typedef struct TipoMove{
 	string nome; 
 	int id;
 	char tipo; //'e' para especial e 'f' para fisico 
-	float power;
+	float power; //quanto poder o move possui para o ataque
 } TipoMove; 
 	
 typedef struct TipoPokemon{
-	TipoChave id;
-	string nome;
+	TipoChave id; //chave - usada para impressao dos pokemon
+	string nome; //nome
 	float atk; //ataque
 	float def; //defesa
 	float hp; //vida
@@ -32,20 +44,23 @@ typedef struct TipoPokemon{
 	TipoMove moves[NUM_MOVES]; //movimentos de ataque possiveis
 } TipoPokemon;
 
-typedef struct TipoNo *TipoApontador;
+typedef struct TipoNo *TipoApontador; //ponteiro para um nó
 
 typedef struct TipoNo {
-	TipoPokemon pokemon;
-	struct TipoNo *prox;
+	TipoPokemon pokemon; //cada nó possui um pokemon
+	struct TipoNo *prox; //e aponta para o proximo no
 } TipoNo;
 
-typedef struct TipoLista {
+typedef struct TipoLista { 
 	TipoApontador Primeiro, Ultimo;
 	int player; //player 1 ou 2
 } TipoLista;
 
 ////////////////////////////////////////////////////////////////////////
 
+
+
+//------------------------------v
 void InicializaMove(TipoMove *mov, string n, int id, char t, float pw){
 	mov->nome = n;
 	mov->id = id;
@@ -218,12 +233,12 @@ void ImprimePokemon(TipoPokemon *poke){
 	
 	cout << endl;
 	cout << "****************************" << endl;
-	cout << "*  NOME:	" <<  poke->nome  << "	           *" << endl;
+	cout << "*  POKEMON:	" <<  poke->nome  << "	           *" << endl;
 //	cout << "*  ID: " << poke->id << "                   *" << endl;
 //	cout << "*  ATK: " << poke->atk << "	           *" << endl;	
 //	cout << "*  DEF: " << poke->def << "	           *" << endl;
 	cout << "*  HP: " << poke->hp << "	           *" << endl;
-	cout << "*  SPEED: " << poke->speed << "	           *" << endl;
+//	cout << "*  SPEED: " << poke->speed << "	           *" << endl;
 //	cout << "*  SP.ATK: " << poke->sp_atk << "	           *" << endl;
 //	cout << "*  SP.DEF:	" << poke->sp_def << " 	   *" << endl;
 //	cout << "*  MOVES: " << "                 *" << endl;
@@ -264,10 +279,12 @@ int Ataque(TipoPokemon *ataca, TipoPokemon *defende, TipoMove *move){
 	//decrementa a hp do pokemon que recebeu o ataque
 	defende->hp = defende->hp - dano; 
 
+	cout << "teste" << endl;
+
 	cout << ataca->nome << " atacou " << defende->nome << " usando " << move->nome << " e causou " << dano << " de dano"<< endl;
 
 	cout << "hp do atacado = " << defende->hp << endl;
-
+	
 	if(defende->hp <= 0){
 		cout << defende->nome << " eliminado!" << endl;
 		//RemoveLista(lista, chave);
@@ -309,41 +326,93 @@ TipoLista comeca(TipoLista p1, TipoLista p2){
 	}
 }
 
+void menuInicial(){
+
+	char jogar;
+
+	std::cout << "\033c";
+
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;             
+	cout << endl;
+
+	//String color = ANSI_COLOR_GREEN;
+
+	printf(ANSI_COLOR_RED "             	                                .::.                         \n"ANSI_COLOR_RESET); 
+	printf(ANSI_COLOR_RED "                                           .;:**'            AMC           	\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_RED "                                           `                  0            \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_RED "               .:XHHHHk.              db.   .;;.     dH  MX   0            \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_RED "             oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN     \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_RED "             QMMMMMb  \"MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_RED "               `MMMM.  )M> :X!Hk. MMMM   XMM.o\"  .  MMMMMMM X?XMMM MMM>!MMP\n"ANSI_COLOR_RESET);
+	printf("                'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `\" MX MMXXMM \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GRAY "                 ~MMMMM~ XMM. .XM XM`\"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GRAY "                  ?MMM>  YMMMMMM! MM   `?MMRb.    `\"\"\"   !L\"MMMMM XM IMMM  \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GRAY "                   MMMX   \"MMMM\"  MM       ~%:           !Mh.\"\"\" dMI IMMP  \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GRAY "                   'MMM.                                             IMX   \n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GRAY "                    ~M!M                                             IMP \n"ANSI_COLOR_RESET);
+	printf("                                                   Console edition :p ");  
+
+
+	//cout << "                                                                 " << endl;
+
+	cout << endl << endl << endl << endl;
+}
+
+
 int main(int argc, char** argv)
 {
 
 //	cout << "Inicializando moves :) " << endl << endl; 
-	TipoMove flamethrower, fire_blast, fly, earthquake, quick_attack, thunderbolt, thunder, skull_bash, ice_beam, hydro_pump, aurora_beam, blizzard; 
+	TipoMove flamethrower, fire_blast, fly, earthquake, quick_attack, thunderbolt, thunder, skull_bash, ice_beam, hydro_pump, aurora_beam, blizzard, surf, body_slam, hyper_beam, take_down, mega_kick, psychic, tri_attack, solar_beam, stomp, egg_bomb; 
 	
 	InicializaMove(&flamethrower, "Flamethrower", 1, 'e', 95); 
 	InicializaMove(&fire_blast, "Fire Blast", 2, 'e', 120);
 	InicializaMove(&fly, "Fly", 3, 'f', 70);
 	InicializaMove(&earthquake, "Earthquake", 4, 'f', 100);
-	
 	InicializaMove(&quick_attack, "Quick Attack", 5, 'f', 40);
 	InicializaMove(&thunderbolt, "Thunderbolt", 6, 'e', 95);
 	InicializaMove(&thunder, "Thunder", 7, 'e', 120);
 	InicializaMove(&skull_bash, "Skull Bash", 8, 'f', 100);
-	
 	InicializaMove(&ice_beam, "Ice Beam", 9, 'e', 95);
-	
 	InicializaMove(&hydro_pump, "Hydro Pump", 10, 'e', 120);
 	InicializaMove(&aurora_beam, "Aurora Beam", 11, 'e', 65);
 	InicializaMove(&blizzard, "Blizzard", 12, 'e', 120);
-
-
+	InicializaMove(&surf, "Surf", 13, 'e', 95);
+	InicializaMove(&body_slam, "Body Slam", 14, 'f', 85);
+	InicializaMove(&hyper_beam, "Hyper Beam", 15, 'e', 150);
+	InicializaMove(&take_down, "Take Down", 16, 'f', 90);
+	InicializaMove(&mega_kick, "Mega Kick", 17, 'f', 120);
+	InicializaMove(&psychic, "Psychic", 18, 'e', 90);
+	InicializaMove(&tri_attack, "Tri Attack", 19, 'e', 80);
+	InicializaMove(&solar_beam, "Solar Beam", 20, 'e', 120);
+	InicializaMove(&stomp, "Stomp", 21, 'f', 65);
+	InicializaMove(&egg_bomb, "Egg Bomb", 22, 'f', 100);
+	
 //	cout << "Inicializando pokemons :) " << endl << endl; 	
 	
-	TipoPokemon pikachu, charizard, dragonite, vaporeon;
+	TipoPokemon pikachu, charizard, dragonite, vaporeon, gyarados, snorlax, mewtwo, exeggutor;
 
 		
-	InicializaPokemon(&pikachu, 1, "Pikachu", 274, 229, 218, 196, 218, 306, quick_attack, thunderbolt, thunder, skull_bash);
+	InicializaPokemon(&pikachu, 25, "Pikachu", 274, 229, 218, 196, 218, 306, quick_attack, thunderbolt, thunder, skull_bash);
 
-	InicializaPokemon(&charizard, 2, "Charizard", 360, 293, 348, 280, 295, 328, flamethrower, fire_blast, fly, earthquake);
+	InicializaPokemon(&charizard, 6, "Charizard", 360, 293, 348, 280, 295, 328, flamethrower, fire_blast, fly, earthquake);
 
-	InicializaPokemon(&dragonite, 3, "Dragonite", 386, 403, 328, 317, 328, 284, fly, earthquake, ice_beam, fire_blast);
+	InicializaPokemon(&dragonite, 149, "Dragonite", 386, 403, 328, 317, 328, 284, fly, earthquake, ice_beam, fire_blast);
 
-	InicializaPokemon(&vaporeon, 4, "Vaporeon", 464, 251, 350, 240, 317, 251, hydro_pump, aurora_beam, quick_attack, blizzard); 
+	InicializaPokemon(&vaporeon, 134, "Vaporeon", 464, 251, 350, 240, 317, 251, hydro_pump, aurora_beam, quick_attack, blizzard); 
+
+	InicializaPokemon(&gyarados, 130, "Gyarados", 394, 383, 240, 282, 328, 287, ice_beam, blizzard, hydro_pump, surf);
+
+	InicializaPokemon(&snorlax, 143, "Snorlax", 524, 350, 251, 251, 350, 174, body_slam,hyper_beam,take_down,mega_kick);
+
+	//InicializaPokemon(&mewtwo, 150, "Mewtwo", 416, 350, 447, 306, 306, 394, psychic,tri_attack, solar_beam,fire_blast);
+
+	InicializaPokemon(&exeggutor, 103, "Exeggutor", 394, 317, 383, 295, 251, 229, stomp,egg_bomb, solar_beam,hyper_beam);
+
 	
 	TipoLista player1, player2;
 
@@ -353,26 +422,27 @@ int main(int argc, char** argv)
 	InsereLista(&player1, charizard);
 	InsereLista(&player1, dragonite);	
 	InsereLista(&player1, pikachu);
+	InsereLista(&player1, vaporeon);	
+	
+	InsereLista(&player2, gyarados);
+	InsereLista(&player2, snorlax);
+	//InsereLista(&player2, mewtwo);
+	InsereLista(&player2, exeggutor);
 
-	InsereLista(&player2, vaporeon);	
-	InsereLista(&player2, pikachu);
-	InsereLista(&player2, dragonite);
 
 	//no começo do jogo, pega os primeiros da lista de cada jogador	
-	
-	TipoPokemon pokemon1 = PrimeiroPokemon(&player1);
-	cout << "primeiro do player1: " << pokemon1.nome << endl;
-	
-	TipoPokemon pokemon2 = PrimeiroPokemon(&player2);
-	cout << "primeiro do player2: " << pokemon2.nome << endl;	
 
-	//TipoPokemon pokemonAtual = comeca(pokemon1, pokemon2);
+	menuInicial();
+	cout << "                                                            Aceita o desafio?              " << endl;
+
+	char entrada;
+	cin >> entrada;
+
+	std::cout << "\033c";
+	
 	
 	TipoLista jogadorAtual = comeca(player1, player2);
 	TipoLista proxJogador, aux;
-	
-	print_pokemon(6); //charizard (?)
-
 
 	bool comeco = true;
 	bool acabou = false;
@@ -387,34 +457,69 @@ int main(int argc, char** argv)
 			proxJogador = player1;
 		}
 
+		cout << "Time 1: " << endl;
+		ImprimeLista(&jogadorAtual);
+		cout << endl;
+
+		cout << "Time 2: " << endl; 
+		ImprimeLista(&proxJogador);
+		cout << endl;
+
+		char jogar;
+		cout << "Iniciar batalha? y/n" << endl;
+		cin >> jogar;
+		std::cout << "\033c";
+		
+		//cout << "=========================================================================" << endl;
+		cout << jogadorAtual.Primeiro->pokemon.nome << " atacando" << endl;
+
+		//imprime ascii
+		print_pokemon(jogadorAtual.Primeiro->pokemon.id);
+		cout << "=========================================================================" << endl;
+
 		TipoMove escolhido;	
+		
+		cout << "Player1" << endl;
+		ImprimePokemon(&player1.Primeiro->pokemon);		
+
+		
+		cout << "Defendendo:" << endl;
+		ImprimePokemon(&player2.Primeiro->pokemon);		
+
 
 		ImprimeMoves(&jogadorAtual.Primeiro->pokemon, &escolhido);
 		
 		Ataque(&jogadorAtual.Primeiro->pokemon, &proxJogador.Primeiro->pokemon, &escolhido);		
+		
 
 		cout << endl;
-
-		//ImprimeLista(&jogadorAtual);
-		//cout << endl;
-		//ImprimeLista(&proxJogador);
-		//cout << endl;
-		
-		//ImprimeLista(&aux);
-		//cout << endl;
 
 		aux = jogadorAtual;
 		jogadorAtual = proxJogador;
 		proxJogador = aux;
 
 		cout << "--------------------------------------------------------------" << endl;
-		
+		std::cout << "\033c";
 		comeco = false;
 	}
 
 	while(!acabou){
 		TipoMove escolhido;	
 		int count = 0;	
+
+		//cout << "=========================================================================" << endl;
+		cout << jogadorAtual.Primeiro->pokemon.nome << " atacando" << endl;
+
+		print_pokemon(jogadorAtual.Primeiro->pokemon.id); //pikachu
+		cout << "=========================================================================" << endl;
+
+		cout << "Player 1" << endl;
+		ImprimePokemon(&player1.Primeiro->pokemon);		
+
+		
+		cout << "Player 2:" << endl;
+		ImprimePokemon(&player2.Primeiro->pokemon);
+
 		ImprimeMoves(&jogadorAtual.Primeiro->pokemon, &escolhido);
 		
 		int eliminado = Ataque(&jogadorAtual.Primeiro->pokemon, &proxJogador.Primeiro->pokemon, &escolhido);		
@@ -423,11 +528,6 @@ int main(int argc, char** argv)
 
 		if(eliminado){
 			RemoveLista(&proxJogador, proxJogador.Primeiro->pokemon.id); 		
-			//aux2 = proxJogador;
-			//proxJogador.Ultimo = aux2.Primeiro;
-			//proxJogador.Primeiro = proxJogador.Primeiro->prox;
-			//count++;
-			//cout << count;
 		}
 
 		cout << endl;
@@ -449,10 +549,17 @@ int main(int argc, char** argv)
 		
 	
 		cout << "--------------------------------------------------------------" << endl;
-	
+		//sleep(5);
+		std::cout << "\033c";
 	
 }
 
+	cout << "acabooooooooooooooou" << endl;
+	menuInicial();
+	cout << "Obrigada por jogar!!" << endl;
+	cout << "Pressione enter parar sair do jogo" << endl; 
+	char fim;
+	cin >> fim;
 	
 	return 0;
 }
